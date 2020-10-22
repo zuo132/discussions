89e6e77aa4fae1f04f8c7ddfbb3be6f4f6d89205
zuo132 <xiez@tcd.ie>
2020-10-22 15:47:34 +0100

    add signup and login

---
+++ routes/auth.js
@@ -0,0 +1,5 @@
+const express = require("express")
+const router = express.Router();
+
+// @desc    Handle user sign up
+// @route   GET /auth/signup
--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,17 @@ router.get("/", (req, res) => {
     res.render("home");
 });
 
+// @desc    Log in page
+// @route   GET /login
+router.get("/login", (req, res) => {
+    res.render("login")
+})
+
+// @desc    Sign up page
+// @route   GET /signup
+router.get("/signup", (req, res) => {
+    res.render("signup")
+})
+
+
 module.exports = router;
